ffc8c42e5ca8c92dffdbd2f3f66c2d9cd611e182
kantemir-imamov <k.imamov@ondigo.de>
2023-08-29 13:11:53 +0200

    cors settings fixed and logging middleware added

---
+++ migrations/image.sql
@@ -0,0 +1,8 @@
+CREATE TABLE IF NOT EXISTS image (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    path TEXT NOT NULL,
+    source TEXT,
+    width INTEGER NOT NULL,
+    height INTEGER NOT NULL
+);
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,14 @@ use actix_multipart::{
     Multipart,
 };
 use actix_web::{
-    get, post,
+    get,
+    middleware::Logger,
+    post,
     web::{self, Data},
     App, HttpResponse, HttpServer, Responder,
 };
-use serde::Deserialize;
+use env_logger::Env;
+use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPool, Pool, Postgres};
 
 #[get("/")]
@@ -42,16 +45,48 @@ async fn random_image(state: Data<AppState>) -> actix_web::Result<impl Responder
 
 #[derive(Debug, MultipartForm)]
 struct ImageFormData {
-    #[multipart(rename = "file")]
-    name: Text<String>,
-    files: Vec<TempFile>,
+    //#[multipart(rename = "file")]
+    title: Text<String>,
+    imagefiles: TempFile,
+}
+
+#[post("/imagetest")]
+async fn upload_image_test(
+    MultipartForm(form): MultipartForm<ImageFormData>,
+) -> actix_web::Result<impl Responder> {
+    //println!("{:?}", form);
+    println!("image post received!");
+    Ok(HttpResponse::Ok().body("uploading image for you!!!"))
+}
+
+#[derive(Debug, MultipartForm)]
+struct TestFormData {
+    title: Text<String>,
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+struct TestFormResponse {
+    title: String,
+}
+
+#[post("/formtest")]
+async fn formtest(
+    MultipartForm(form): MultipartForm<TestFormData>,
+) -> actix_web::Result<impl Responder> {
+    println!("image post received!");
+    let res = TestFormResponse {
+        title: form.title.into_inner(),
+    };
+    Ok(HttpResponse::Ok().json(res))
 }
 
 #[post("/image")]
 async fn upload_image(
     state: Data<AppState>,
-    MultipartForm(form): MultipartForm<ImageFormData>,
+    //MultipartForm(form): MultipartForm<ImageFormData>,
 ) -> actix_web::Result<impl Responder> {
+    //println!("{:?}", form);
+    println!("image post received!");
     Ok(HttpResponse::Ok().body("uploading image for you!!!"))
 }
 
@@ -74,17 +109,31 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to Postgres.");
 
+    let host = "127.0.0.1";
+    let port = 5000;
+
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
+
+    println!(
+        "starting server at adress {}:{} or http://localhost:{port}",
+        host, port
+    );
     HttpServer::new(move || {
         let cors = Cors::default().allow_any_origin();
 
         App::new()
+            .wrap(cors)
+            .wrap(Logger::default())
             .app_data(db.clone())
             .service(hello)
             .service(echo)
             .service(random_image)
             .route("/hey", web::get().to(manual_hello))
+            .service(upload_image)
+            .service(upload_image_test)
+            .service(formtest)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
