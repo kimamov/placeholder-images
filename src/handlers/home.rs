522cefe6804e4e755636720de91fc226d0fd39e9
Kantemir Imamov <imamov@skygate.de>
2023-11-15 15:07:12 +0100

    [TASK] added serverside templating

--- src/handlers/home.rs
+++ src/handlers/home.rs
@@ -1,8 +1,24 @@
 use actix_web::{get, HttpResponse, Responder};
+use ramhorns::{Template, Content};
 
 use crate::types::AppStateData;
 
+#[derive(Content)]
+struct Home {
+    //posts: Vec<String>
+    title: String,
+}
+
 #[get("/")]
 pub async fn index(state: AppStateData) -> actix_web::Result<impl Responder> {
-    Ok(HttpResponse::Ok().body("welcome to the image api"))
+
+    let source="<h1>{{title}}</h1><h2>welcome to my image service</h2>";
+
+    let tpl=Template::new(source).unwrap();
+
+    let rendered=tpl.render(&Home {title: "Test passed".to_string()});
+
+
+    Ok(HttpResponse::Ok().body(rendered))
 }
+
